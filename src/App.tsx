8f2fecf73e785fa01f635277d2a16847d597dd60
Hossein Salari <hossein.salari002@gmail.com>
2024-05-03 17:26:01 +0330

    feat: implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
 
+// pages
+import ProfilePage from "./pages/ProfilePage";
+import SignupPage from "./pages/SignUpPage";
+import EditProfilePage from "./pages/EditProfilePage";
+
+function App() {
   return (
-   <div className="text-blue-500">test</div>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<ProfilePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/edit-profile" element={<EditProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
