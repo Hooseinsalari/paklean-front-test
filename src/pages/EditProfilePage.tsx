2d46e1e23c573da3aa4c4793119140d58a92dba4
Hossein Salari <hossein.salari002@gmail.com>
2024-05-05 00:55:01 +0330

    feat: implement edit page haeder

--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import Header from "../components/templates/EditProfile/Header";
+import EditProfileForm from "../components/templates/EditProfile/EditProfileForm";
 
 const EditProfilePage = () => {
-    return (
-        <div>
-            EditProfilePage
-        </div>
-    );
+  return (
+    <div className="bg-primary w-full px-4 py-6 h-screen">
+      <Header />
+
+      <EditProfileForm />
+    </div>
+  );
 };
 
 export default EditProfilePage;
